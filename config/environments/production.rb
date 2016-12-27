8be871fc74321ad3f8e52ff528a26b51f95da8c3
Kairav Parekh <kparekh01@gmail.com>
2016-12-27 17:34:31 -0500

    changing host to actual link of app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { :host => 'https://dress-your-best.herokuapp.com/'}
-  config.action_mailer.asset_host = 'https://dress-your-best.herokuapp.com/'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
